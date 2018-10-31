2799cdd0330d1bf4bc446d4d4760aa9e802ae0d9
BhavaniChoudary <lakshmi.papasani96@msitprogram.net>
2018-10-31 23:30:57 +0530

    "ads2_m2_a1_1: 24/24 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2-assignment/m2/2.1 CC_Percolation/Solution.java
+++ ADS-2-assignment/m2/2.1 CC_Percolation/Solution.java
@@ -1,8 +1,19 @@
 import java.util.Scanner;
+/**
+ * class Solution.
+ */
 public final class Solution {
+    /**
+     * Constructs the object.
+     */
     private Solution() {
         //function.
     }
+    /**
+     * Main function_description.
+     *
+     * @param      args  The arguments
+     */
     public static void main(final String[] args) {
         Scanner scan = new Scanner(System.in);
         int number = Integer.parseInt(scan.nextLine());
