f6c8396b899dd88eabe928f953553e095d1e828f
BhavaniChoudary <lakshmi.papasani96@msitprogram.net>
2018-11-02 23:50:45 +0530

    "ads2_m3_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignment/m3/3.1 Directed Cycle/Stack.java
+++ ADS-2-assignment/m3/3.1 Directed Cycle/Stack.java
@@ -76,14 +76,22 @@ public class Stack<Item> implements Iterable<Item> {
         n--;
         return item;
     }
-
+    /**
+     * returns the item recently added to stack.
+     *
+     * @return     { description_of_the_return_value }
+     */
     public Item peek() {
         if (isEmpty()) {
             throw new RuntimeException("Stack underflow");
         }
         return first.item;
     }
-
+    /**
+     * Returns a string representation of the object.
+     *
+     * @return     String representation of the object.
+     */
     public String toString() {
         StringBuilder s = new StringBuilder();
         for (Item item : this) {
@@ -91,18 +99,33 @@ public class Stack<Item> implements Iterable<Item> {
         }
         return s.toString();
     }
-
+    /**
+     * iterator.
+     *
+     * @return     { description_of_the_return_value }
+     */
     public Iterator<Item> iterator() {
         return new ListIterator();
     }
+    /**
+     * Class for list iterator.
+     */
     private class ListIterator implements Iterator<Item> {
         private Node current = first;
         public boolean hasNext() {
             return current != null;
         }
+        /**
+         * remove function_description.
+         */
         public void remove() {
             throw new UnsupportedOperationException();
         }
+        /**
+         * next function_description.
+         *
+         * @return     { description_of_the_return_value }
+         */
         public Item next() {
             if (!hasNext()) {
                 throw new NoSuchElementException();
