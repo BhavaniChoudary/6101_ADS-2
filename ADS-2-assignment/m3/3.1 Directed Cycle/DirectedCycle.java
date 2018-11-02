6ace129c97528c9b4c9c31ebb692b55800b29c12
BhavaniChoudary <lakshmi.papasani96@msitprogram.net>
2018-11-02 23:45:55 +0530

    "ads2_m3_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignment/m3/3.1 Directed Cycle/DirectedCycle.java
+++ ADS-2-assignment/m3/3.1 Directed Cycle/DirectedCycle.java
@@ -1,8 +1,28 @@
+/**
+ * Class for directed cycle.
+ */
 public class DirectedCycle {
+    /**
+     * marked var_description.
+     */
     private boolean[] marked;
+    /**
+     * edgeTo var_description.
+     */
     private int[] edgeTo;
+    /**
+     * onStack var_description.
+     */
     private boolean[] onStack;
+    /**
+     * directed cycle var_description.
+     */
     private Stack<Integer> cycle;
+    /**
+     * Constructs the object.
+     *
+     * @param      g     { parameter_description }
+     */
     public DirectedCycle(final Digraph g) {
         marked  = new boolean[g.vertex()];
         onStack = new boolean[g.vertex()];
@@ -13,6 +33,12 @@ public class DirectedCycle {
             }
         }
     }
+    /**
+     * dfs function_description.
+     *
+     * @param      g     { parameter_description }
+     * @param      v     { parameter_description }
+     */
     private void dfs(final Digraph g, final int v) {
         onStack[v] = true;
         marked[v] = true;
@@ -34,12 +60,27 @@ public class DirectedCycle {
         }
         onStack[v] = false;
     }
+    /**
+     * Determines if it has cycle.
+     *
+     * @return     True if has cycle, False otherwise.
+     */
     public boolean hasCycle() {
         return cycle != null;
     }
+    /**
+     * Iterator.
+     *
+     * @return     { description_of_the_return_value }
+     */
     public Iterable<Integer> cycle() {
         return cycle;
     }
+    /**
+     * check function_description.
+     *
+     * @return     { description_of_the_return_value }
+     */
     private boolean check() {
         if (hasCycle()) {
             int first = -1, last = -1;
