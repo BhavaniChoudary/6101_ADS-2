75da2ed5ba4e6582d956b74c46142d37496391e7
BhavaniChoudary <lakshmi.papasani96@msitprogram.net>
2018-11-03 12:59:21 +0530

    "ads2_m6_a1_1: 0/12 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignment/m6/ADS-2Week-1/Solution.java
+++ ADS-2-assignment/m6/ADS-2Week-1/Solution.java
@@ -3,60 +3,55 @@ import java.util.Scanner;
  * Class for page rank.
  */
 class PageRank {
-	
+    //class.
 }
 /**
  * Class for web search.
  */
 class WebSearch {
-
+    //class.
 }
 /**
  * Class for solution.
  */
 public class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
         //function.
     }
-	/**
-	 * main function_description.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int totalVertices = Integer.parseInt(sc.nextLine());
-		Digraph d = new Digraph(totalVertices);
-		int i = 0;
-		while (i < totalVertices) {
-			String[] arra1 = sc.nextLine().split(" ");
-			for (int j = 1;j < arra1.length; j++) {
-				d.addEdge(Integer.parseInt(arra1[0]),
-					Integer.parseInt(arra1[j]));
-			}
-			i++;
-		}	
-		System.out.println(d.toString());	
-		// Create page rank object and pass the graph object to the constructor
-		
-		// print the page rank object
-		
-		// This part is only for the final test case
-		
-		// File path to the web content
-		String file = "WebContent.txt";
-		
-		// instantiate web search object
-		// and pass the page rank object and the file path to the constructor
-		
-		// read the search queries from std in
-		// remove the q= prefix and extract the search word
-		// pass the word to iAmFeelingLucky method of web search
-		// print the return value of iAmFeelingLucky
-		
-	}
+    /**
+     * main function_description.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int totalVertices = Integer.parseInt(sc.nextLine());
+        Digraph d = new Digraph(totalVertices);
+        int i = 0;
+        while (i < totalVertices) {
+            String[] arra1 = sc.nextLine().split(" ");
+            for (int j = 1; j < arra1.length; j++) {
+                d.addEdge(Integer.parseInt(arra1[0]),
+                    Integer.parseInt(arra1[j]));
+            }
+            i++;
+        }   
+        System.out.println(d.toString());
+        //Create page rank object and pass the graph object to the constructor
+        // print the page rank object
+        // This part is only for the final test case
+        // File path to the web content
+        String file = "WebContent.txt";
+        // instantiate web search object
+        // and pass the page rank object and the file path to the constructor
+        // read the search queries from std in
+        // remove the q= prefix and extract the search word
+        // pass the word to iAmFeelingLucky method of web search
+        // print the return value of iAmFeelingLucky
+        
+    }
 }
 
