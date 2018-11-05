06179614266a97cdcb283bc6e70a24ac4fed1e25
BhavaniChoudary <lakshmi.papasani96@msitprogram.net>
2018-11-05 17:53:27 +0530

    "ads2_m3_a2_1: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignment/m3/3.2 Bipartite/Graph.java
+++ ADS-2-assignment/m3/3.2 Bipartite/Graph.java
@@ -1,7 +1,21 @@
 public class Graph {
+    /**
+     * vertices var_description.
+     */
     private int vertices;
+    /**
+     * edges var_description.
+     */
     private int edges;
+    /**
+     * adj var_description.
+     */
     private Bag<Integer>[] adj;
+    /**
+     * Constructs the object.
+     *
+     * @param      v     { parameter_description }
+     */
     public Graph(final int v) {
         if (v < 0) {
             throw new RuntimeException(
@@ -14,17 +28,40 @@ public class Graph {
             adj[i] = new Bag<Integer>();
         }
     }
+    /**
+     * vertex.
+     *
+     * @return     { description_of_the_return_value }
+     */
     public int vertex() {
         return vertices;
     }
+    /**
+     * edge.
+     *
+     * @return     { description_of_the_return_value }
+     */
     public int edge() {
         return edges;
     }
+    /**
+     * Adds an edge.
+     *
+     * @param      v     { parameter_description }
+     * @param      w     { parameter_description }
+     */
     public void addEdge(final int v, final int w) {
         edges++;
         adj[v].add(w);
         adj[w].add(v);
     }
+    /**
+     * iterator.
+     *
+     * @param      v     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
     public Iterable<Integer> adj(final int v) {
         return adj[v];
     }
