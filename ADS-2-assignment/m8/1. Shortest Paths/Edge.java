084951c982b5b5b5669549442e9c27a1f5b13733
BhavaniChoudary <lakshmi.papasani96@msitprogram.net>
2018-11-09 11:55:51 +0530

    "ads2_m8_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignment/m8/1. Shortest Paths/Edge.java
+++ ADS-2-assignment/m8/1. Shortest Paths/Edge.java
@@ -1,24 +1,71 @@
+/**
+ * Class for edge.
+ */
 class Edge implements Comparable<Edge> {
+	/**
+	 * edge1 int variable.
+	 */
 	private int edge1;
+	/**
+	 * edge2 int variable.
+	 */
 	private int edge2;
+	/**
+	 * weight.
+	 */
 	private int weight;
+	/**
+	 * Constructs the object.
+	 *
+	 * @param      one   One
+	 * @param      two   Two
+	 * @param      w     { parameter_description }
+	 */
 	Edge(int one, int two, int w) {
 		this.edge1 = one;
         this.edge2 = two;
         this.weight = w;
 	}
+	/**
+	 * Gets the weight.
+	 * complexity is O(1)
+	 *
+	 * @return     The weight.
+	 */
 	public int getWeight() {
         return this.weight;
     }
+    /**
+     * gets other edge connected.
+     * complexity is O(1)
+     *
+     * @param      one   One
+     *
+     * @return     { description_of_the_return_value }
+     */
     public int other(final int one) {
         if (one == edge1) {
             return this.edge2;
         }
         return this.edge1;
     }
+    /**
+     * returns edge.
+     * complexity is O(1)
+     *
+     * @return     { description_of_the_return_value }
+     */
     public int either() {
     	return this.edge1;
     }
+    /**
+     * compares.
+     * complexity is O(1)
+     *
+     * @param      that  The that
+     *
+     * @return     { description_of_the_return_value }
+     */
     public int compareTo(final Edge that) {
         if (this.weight < that.weight) {
             return -1;
