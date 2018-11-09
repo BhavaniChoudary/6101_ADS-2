ac2a1c075f626aaeac9df52661c1f92ad27fa4fb
BhavaniChoudary <lakshmi.papasani96@msitprogram.net>
2018-11-09 18:24:02 +0530

    "ads2_m8_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignment/m8/1. Shortest Paths/Queue.java
+++ ADS-2-assignment/m8/1. Shortest Paths/Queue.java
@@ -1,27 +1,77 @@
+/**
+ * List of Queue.
+ *
+ * @param      <item>  The item
+ */
 public class Queue<item> {
+    /**
+     * no.of elements.
+     */
 	private int n;
+    /**
+     * beggining of queue.
+     */
 	private Node first;
+    /**
+     * end of queue.
+     */
 	private Node last;
+    /**
+     * Class for node.
+     */
 	private class Node {
+        /**
+         * item.
+         */
 		private Item item;
+        /**
+         * next.
+         */
 		private Node next;
 	}
+    /**
+     * Constructs the object.
+     */
 	Queue() {
 		first = null;
 		last = null;
 	}
+    /**
+     * Determines if empty.
+     * complexity O(1)
+     *
+     * @return     True if empty, False otherwise.
+     */
 	public boolean isEmpty() {
 		return first == null;
 	}
+    /**
+     * size.
+     * complexity O(1)
+     *
+     * @return     { description_of_the_return_value }
+     */
 	public int size() {
 		return n;
 	}
+    /**
+     * return the item least recently added to the queue.
+     * complexity O(1)
+     *
+     * @return     { description_of_the_return_value }
+     */
 	public Item peek() {
         if (isEmpty()) {
             throw new RuntimeException("Queue underflow");
         }
         return first.item;
     }
+    /**
+     * add the item.
+     * complexity O(1)
+     *
+     * @param      item  The item
+     */
     public void enqueue(final Item item) {
         Node x = new Node();
         x.item = item;
@@ -34,6 +84,12 @@ public class Queue<item> {
         }
         n++;
     }
+    /**
+     * remove item in queue.
+     * complexity O(1)
+     *
+     * @return     { description_of_the_return_value }
+     */
     public Item dequeue() {
         if (isEmpty()) {
             throw new RuntimeException(
