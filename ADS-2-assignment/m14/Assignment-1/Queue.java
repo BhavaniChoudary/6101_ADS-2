af6ee59392637234d80101ff50e44182d911d2fe
BhavaniChoudary <lakshmi.papasani96@msitprogram.net>
2018-11-15 09:37:23 +0530

    "ads2_m14_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2-assignment/m14/Assignment-1/Queue.java
+++ ADS-2-assignment/m14/Assignment-1/Queue.java
@@ -1,26 +1,78 @@
 import java.util.Iterator;
+/**
+ * List of Queue.
+ *
+ * @param      <Item>  The item
+ */
 public class Queue<Item> implements Iterable<Item> {
+    /**
+     * beggining of Queue.
+     */
     private Node<Item> first;
+    /**
+     * end of queue.
+     */
     private Node<Item> last;
+    /**
+     * no.of elements.
+     */
     private int n;
+    /**
+     * Class for node.
+     *
+     * @param      <Item>  The item
+     */
     private static class Node<Item> {
+        /**
+         * item.
+         */
         private Item item;
+        /**
+         * next.
+         */
         private Node<Item> next;
     }
+    /**
+     * Constructs the object.
+     */
     Queue() {
         first = null;
         last  = null;
         n = 0;
     }
+    /**
+     * Determines if empty.
+     * complexity O(1)
+     *
+     * @return     True if empty, False otherwise.
+     */
     public boolean isEmpty() {
         return first == null;
     }
+    /**
+     * Returns the number of items in the queue.
+     * complexity O(1)
+     *
+     * @return     { description_of_the_return_value }
+     */
     public int size() {
         return n;
     }
+    /**
+     * return last recently added item.
+     * complexity O(1)
+     *
+     * @return     { description_of_the_return_value }
+     */
     public Item peek() {
         return first.item;
     }
+    /**
+     * Adds the item to this queue.
+     * complexity O(1)
+     *
+     * @param      item  The item
+     */
     public void enqueue(final Item item) {
         Node<Item> oldlast = last;
         last = new Node<Item>();
@@ -33,6 +85,12 @@ public class Queue<Item> implements Iterable<Item> {
         }
         n++;
     }
+    /**
+     * Removes and returns the item on this queue that was least recently added.
+     * complexity O(1)
+     *
+     * @return     { description_of_the_return_value }
+     */
     public Item dequeue() {
         Item item = first.item;
         first = first.next;
@@ -42,6 +100,12 @@ public class Queue<Item> implements Iterable<Item> {
         }
         return item;
     }
+    /**
+     * Returns a string representation of the object.
+     * complexity O(1)
+     *
+     * @return     String representation of the object.
+     */
     public String toString() {
         StringBuilder s = new StringBuilder();
         for (Item item : this) {
@@ -50,20 +114,54 @@ public class Queue<Item> implements Iterable<Item> {
         }
         return s.toString();
     }
+    /**
+     * Iterator.
+     *
+     * @return     { description_of_the_return_value }
+     */
     public Iterator<Item> iterator()  {
         return new ListIterator<Item>(first);
     }
+    /**
+     * Class for list iterator.
+     *
+     * @param      <Item>  The item
+     */
     private class ListIterator<Item> implements Iterator<Item> {
+        /**
+         * current node.
+         */
         private Node<Item> current;
+        /**
+         * Constructs the object.
+         *
+         * @param      first1  The first 1
+         */
         ListIterator(final Node<Item> first1) {
             current = first1;
         }
+        /**
+         * Determines if it has next.
+         * complexity O(1)
+         *
+         * @return     True if has next, False otherwise.
+         */
         public boolean hasNext() {
             return current != null;
         }
+        /**
+         * remove method.
+         * complexity O(1)
+         */
         public void remove() {
             throw new UnsupportedOperationException();
         }
+        /**
+         * method for finding next.
+         * complexity O(1)
+         *
+         * @return     { description_of_the_return_value }
+         */
         public Item next() {
             Item item = current.item;
             current = current.next;
