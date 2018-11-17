902f471fdcc2f2b97f9a44a3eb0f69d6e16b555c
BhavaniChoudary <lakshmi.papasani96@msitprogram.net>
2018-11-17 13:10:36 +0530

    "ads2_m18_a1_1: 2/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignment/m18/ADS-2Exam-3/Solution.java
+++ ADS-2-assignment/m18/ADS-2Exam-3/Solution.java
@@ -146,7 +146,7 @@ class T9 {
      *
      * @param      st    { parameter_description }
      */
-    public T9(final BinarySearchST<String, Integer> st) {
+    protected T9(final BinarySearchST<String, Integer> st) {
         // your code goes here
         t = new TST<Integer>();
         for (String str : st.keys()) {
